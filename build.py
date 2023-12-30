8a33cde9b1d813c993e18d777f2b4b328a2f80fc
manuelseeger <mail@manuelseeger.de>
2023-12-30 14:39:21 +0100

    Add token count to build

--- build.py
+++ build.py
@@ -2,11 +2,14 @@ from aicoach.functions import AIFunctions
 import os
 from dotenv import load_dotenv
 import json
+import tiktoken
 
 load_dotenv()
 
 
 def main():
+    encoding = tiktoken.get_encoding("cl100k_base")
+
     tools = [
         {
             "type:": "code_interpreter",
@@ -16,6 +19,8 @@ def main():
 
     with open(os.path.join("aicoach", "initial_instructions.txt"), "r") as f:
         assistant["instructions"] = f.read()
+        tokens = encoding.encode(assistant["instructions"])
+        print(f"Current tokens in initial instructions: {len(tokens)}")
 
     assistant["tools"] = assistant["tools"] + [
         {"type": "function", "function": f.json()} for f in AIFunctions
