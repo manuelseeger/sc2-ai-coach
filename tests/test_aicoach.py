e0a79c5818501ea28d13bbe6a65e86bf9f1f933d
manuelseeger <mail@manuelseeger.de>
2023-12-30 14:39:06 +0100

    add test for query db

--- tests/test_aicoach.py
+++ tests/test_aicoach.py
@@ -3,13 +3,26 @@ import pytest
 from aicoach import AICoach
 
 
-def test_function_call_smurf_detection():
+def test_function_smurf_detection():
     aicoach = AICoach()
     aicoach.create_thread()
 
     handle = "2-S2-1-691545"
 
     message = f"I am playing someone on around 3000 MMR. The player I am playing with has the handle toon handle '{handle}'. Can you tell me if they are a smurf?"
+
+    response = aicoach.chat(message)
+
+    assert isinstance(response, str)
+    assert len(response) > 0
+    print(response)
+
+
+def test_function_query_build_order():
+    aicoach = AICoach()
+    aicoach.create_thread()
+
+    message = f"My player ID is 'zatic'. Get the build order of the opponent of the last 5 games I played against 'protoss' opponents."
 
     response = aicoach.chat(message)
 
