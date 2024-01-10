7a39c031563d8acec1ddf377e248999d2fb63d4e
manuelseeger <mail@manuelseeger.de>
2024-01-10 09:08:20 +0100

    Harden lookup skill

--- aicoach/functions/LookupPlayer.py
+++ aicoach/functions/LookupPlayer.py
@@ -1,6 +1,10 @@
 from .base import AIFunction
 from typing import Annotated
 import requests
+from logging import getLogger
+from config import config
+
+log = getLogger(config.name)
 
 PROFILE_BASE = "https://starcraft2.com/en-us/profile/"
 API_BASE = "https://starcraft2.blizzard.com/en-us/api/sc2/profile/"
@@ -9,28 +13,33 @@ API_BASE = "https://starcraft2.blizzard.com/en-us/api/sc2/profile/"
 @AIFunction
 def LookupPlayer(
     toon_handle: Annotated[
-        str, "The unique handle of a player on Battle.net ('toon_handle'). A handle has the format N-SN-N-NNNNNNN where N are all numeric. Example: 2-S2-1-1849098"
+        str,
+        "The unique handle of a player on Battle.net ('toon_handle'). A handle has the format N-SN-N-NNNNNNN where N are all numeric. Example: 2-S2-1-1849098",
     ]
 ) -> str:
     """Lookup a player by their toon handle and return their career summary in JSON format.
 
     These stats can be used to determine if a player is a smurf or not based on the current MMR
     and the history of the player.
-    
+
     Only call this function with valid toon handles in the format 2-S2-1-1849098.
     """
     summary = {}
+    try:
+        handle = toon_handle.replace("-S2-", "/").replace("-", "/")
 
-    handle = toon_handle.replace("-S2-", "/").replace("-", "/")
+        api_url = API_BASE + handle
 
-    api_url = API_BASE + handle
+        with requests.Session() as s:
+            r = s.get(api_url)
 
-    with requests.Session() as s:
-        r = s.get(api_url)
+            profile = r.json()
 
-        profile = r.json()
+            summary["career"] = profile["career"]
+            summary["snapshot"] = profile["snapshot"]
 
-        summary["career"] = profile["career"]
-        summary["snapshot"] = profile["snapshot"]
-
+            return summary
+    except Exception as e:
+        log.error(e)
+    finally:
         return summary
