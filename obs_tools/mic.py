22d181b2f488565d3f14fe69adec161d123dc3d7
manuelseeger <mail@manuelseeger.de>
2024-03-02 12:45:30 +0100

    Simplify voice output

--- obs_tools/mic.py
+++ obs_tools/mic.py
@@ -25,12 +25,6 @@ class Microphone:
 
         self.microphone = sr.Microphone(device_index=self.device_index)
 
-        self.voiceengine = pyttsx3.init()
-        voices = self.voiceengine.getProperty("voices")
-        self.voiceengine.setProperty(
-            "voice", voices[3].id if len(voices) > 3 else voices[0].id
-        )
-
     def listen(self):
         with self.microphone as source:
             audio = self.recognizer.listen(source)
@@ -47,7 +41,28 @@ class Microphone:
         Popen(
             [
                 sys.executable,
-                "obs_tools/say.py",
+                "obs_tools/mic.py",
                 text,
             ]
         )
+
+
+def init_voice_engine():
+    engine = pyttsx3.init()
+    voices = engine.getProperty("voices")
+    engine.setProperty("voice", voices[3].id if len(voices) > 3 else voices[0].id)
+    return engine
+
+
+def say(s):
+    engine.say(s)
+    engine.runAndWait()  # In here the program will wait as if is in main file
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        engine = init_voice_engine()
+
+        say(str(sys.argv[1]))
+    else:
+        print("Usage: python3 mic.py <text>")
