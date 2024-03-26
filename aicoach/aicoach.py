66d3029773a80d719e4d937a3a34ee6fa9e85bc6
Manuel Seeger <Manuel.Seeger@swisslife.ch>
2024-03-26 11:40:25 +0100

    Streaming event handler stub

--- aicoach/aicoach.py
+++ aicoach/aicoach.py
@@ -7,7 +7,22 @@ from .functions import AIFunctions
 from config import config
 import logging
 from typing import Dict
+from openai import AssistantEventHandler
+from typing_extensions import override
 
+#https://platform.openai.com/docs/assistants/overview?context=with-streaming
+class EventHandler(AssistantEventHandler):    
+  @override
+  def on_text_created(self, text) -> None:
+    print(f"\nassistant > ", end="", flush=True)
+      
+  @override
+  def on_text_delta(self, delta, snapshot):
+    print(delta.value, end="", flush=True)
+      
+  def on_tool_call_created(self, tool_call):
+    print(f"\nassistant > {tool_call.type}\n", flush=True)
+    
 
 log = logging.getLogger(f"{config.name}.{__name__}")
 log.setLevel(logging.INFO)
