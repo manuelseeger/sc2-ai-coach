3f65f23772125d91b3368dfba0344714de005549
Manuel Seeger <Manuel.Seeger@swisslife.ch>
2024-03-26 10:19:35 +0100

    Dedicated metadata test file

--- tests/test_replaydb.py
+++ tests/test_replaydb.py
@@ -1,8 +1,6 @@
-from replays import ReplayReader, time2secs, db, replaydb
+from replays import ReplayReader, time2secs
 from os.path import join
-from replays.types import AssistantMessage, Metadata, Role
-from datetime import datetime, timedelta
-from pyodmongo.queries import eq
+
 
 FIXTURE_DIR = "tests/fixtures"
 
@@ -74,33 +72,3 @@ def test_default_projection_workers():
     ]
     assert all(worker_in_bo)
 
-
-def test_add_metadata():
-    reader = ReplayReader()
-
-    rep = "Site Delta LE (106) ZvZ 2base Muta into mass muta chaotic win.SC2Replay"
-
-    replay = reader.load_replay(join(FIXTURE_DIR, rep))
-
-    meta = Metadata(
-        replay=replay.id,
-    )
-    meta.description = "This is a test description 3"
-    meta.conversation = [
-        AssistantMessage(
-            **{"created_at": datetime.now(), "role": Role.user, "text": "Hello"}
-        ),
-        AssistantMessage(
-            **{
-                "created_at": datetime.now() + timedelta(seconds=1),
-                "role": Role.assistant,
-                "text": "Hi there!",
-            }
-        ),
-    ]
-
-    meta.tags = ["test", "zvz", "muta"]
-
-    replaydb.db.save(meta, query=eq(Metadata.replay, replay.id))
-
-    db.save(meta, query=eq(Metadata.replay, replay.id))
