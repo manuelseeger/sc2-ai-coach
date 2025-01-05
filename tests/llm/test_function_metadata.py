278e4f56c3ca76dc1ed335a923fd77a67a5cfc48
manuelseeger <mail@manuelseeger.de>
2025-01-05 12:16:41 +0100

    Test for tagging players

--- tests/llm/test_function_metadata.py
+++ tests/llm/test_function_metadata.py
@@ -67,3 +67,42 @@ def test_add_tag_after_replay_summary(replay_file, util):
     assert isinstance(response, str)
     assert len(response) > 0
     print(response)
+
+
+@pytest.mark.parametrize(
+    "replay_file",
+    [
+        "Site Delta LE (106) ZvZ 2base Muta into mass muta chaotic win.SC2Replay",
+    ],
+    indirect=True,
+)
+def test_add_player_tag_after_replay(replay_file, util):
+    coach = AICoach()
+
+    reader = ReplayReader()
+
+    replay = reader.load_replay(replay_file)
+
+    opponent = replay.get_player(config.student.name, opponent=True).name
+    replacements = {
+        "student": str(config.student.name),
+        "map": str(replay.map_name),
+        "opponent": str(opponent),
+        "replay": str(replay.default_projection_json(limit=600, include_workers=False)),
+    }
+
+    prompt = Templates.new_replay.render(replacements)
+
+    thread_id = coach.create_thread(prompt)
+
+    response = util.stream_thread(coach)
+
+    message = f"Can you please tag this player as a smurf?"
+
+    response = ""
+    for token in coach.chat(message):
+        response += token
+
+    assert isinstance(response, str)
+    assert len(response) > 0
+    print(response)
