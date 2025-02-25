62d7ce913a5a3f41473b627871adfeffa24ef2c7
manuelseeger <mail@manuelseeger.de>
2025-02-25 22:03:02 +0100

    Get   map stats  from DB

--- src/mapstats.py
+++ src/mapstats.py
@@ -1,59 +1,16 @@
 import logging
 from functools import cached_property
-from typing import Any, ClassVar, Dict
-from urllib.parse import urljoin, urlparse, urlunparse
+from typing import Any, ClassVar
+from urllib.parse import urlparse, urlunparse
 
-import httpx
-from bs4 import BeautifulSoup
-from Levenshtein import distance as levenshtein
-from pydantic import BaseModel, HttpUrl, computed_field
+from pydantic import HttpUrl, computed_field
 from pyodmongo import DbModel, MainBaseModel
 
 from config import config
+from src.replaydb.db import replaydb
+from src.replaydb.types import Replay
 
 log = logging.getLogger(f"{config.name}.{__name__}")
-
-
-def add_path_segment(url: HttpUrl, *segments: Any) -> str:
-    parsed_url = urlparse(str(url))
-
-    new_path = "/".join(
-        [parsed_url.path.rstrip("/")] + [str(s) for s in list(segments)]
-    )
-
-    updated_url = urlunparse(parsed_url._replace(path=new_path))
-    return updated_url
-
-
-def get_map_stats(map):
-    if config.student.sc2replaystats_map_url is None:
-        return None
-    with httpx.Client() as s:
-
-        url = add_path_segment(
-            config.student.sc2replaystats_map_url, config.season, config.student.race[0]
-        )
-
-        try:
-            r = s.get(url)
-            soup = BeautifulSoup(r.content, "html.parser")
-
-            h2s = soup("h2")
-
-            for h2 in h2s:
-                if levenshtein(h2.string.lower(), map.lower()) < 5:
-                    for sibling in h2.parent.next_siblings:
-                        if sibling.name == "table":
-                            return sibling
-        except Exception as e:
-            log.warning(f"Could not get map stats: {e}")
-
-
-def update_map_stats(map):
-    stats = get_map_stats(map)
-    if stats is not None:
-        with open("obs/map_stats.html", "w") as f:
-            f.write(stats.prettify())
 
 
 class Matchup(MainBaseModel):
@@ -80,7 +37,7 @@ class MatchupsByMap(DbModel):
             "$project": {
                 "map_name": 1,
                 "players": 1,
-                "theplayer": {
+                "student": {
                     "$arrayElemAt": [
                         "$players",
                         {"$indexOfArray": ["$players.name", config.student.name]},
@@ -110,21 +67,21 @@ class MatchupsByMap(DbModel):
                 },
             }
         },
-        {"$match": {"$expr": {"$eq": ["$theplayer.play_race", config.student.race]}}},
+        {"$match": {"$expr": {"$eq": ["$student.play_race", config.student.race]}}},
         {
             "$group": {
                 "_id": {
                     "map_name": "$map_name",
                     "matchup": {
-                        "$concat": ["$theplayer.play_race", "v", "$opponent.play_race"]
+                        "$concat": ["$student.play_race", "v", "$opponent.play_race"]
                     },
                 },
                 "totalGames": {"$sum": 1},
                 "wins": {
-                    "$sum": {"$cond": [{"$eq": ["$theplayer.result", "Win"]}, 1, 0]}
+                    "$sum": {"$cond": [{"$eq": ["$student.result", "Win"]}, 1, 0]}
                 },
                 "losses": {
-                    "$sum": {"$cond": [{"$eq": ["$theplayer.result", "Loss"]}, 1, 0]}
+                    "$sum": {"$cond": [{"$eq": ["$student.result", "Loss"]}, 1, 0]}
                 },
             }
         },
@@ -152,3 +109,29 @@ class MatchupsByMap(DbModel):
         },
         {"$project": {"_id": 0, "map": "$_id", "matchups": 1}},
     ]
+
+
+def add_path_segment(url: HttpUrl, *segments: Any) -> str:
+    parsed_url = urlparse(str(url))
+
+    new_path = "/".join(
+        [parsed_url.path.rstrip("/")] + [str(s) for s in list(segments)]
+    )
+
+    updated_url = urlunparse(parsed_url._replace(path=new_path))
+    return updated_url
+
+
+def update_map_stats(map):
+    pass
+
+
+def get_season_map_stats(map: str) -> MatchupsByMap | None:
+    q = (Replay.map_name == map) & (Replay.date >= config.season_start)
+
+    maps: list[MatchupsByMap] = replaydb.db.find_many(Model=MatchupsByMap, query=q)
+
+    if len(maps) > 0:
+        return maps[0]
+    else:
+        return None
--- tests/integration/test_mapstats.py
+++ tests/integration/test_mapstats.py
@@ -2,28 +2,33 @@ import pytest
 from rich import print
 
 from config import config
-from src.mapstats import MatchupsByMap, get_map_stats
+from src.mapstats import MatchupsByMap, get_season_map_stats
 from src.replaydb.db import replaydb
-from src.replaydb.types import Player, Replay
+from src.replaydb.types import Replay
 
 
 @pytest.mark.parametrize(
     "map_name",
     config.ladder_maps,
 )
-def test_save_map_stats(map_name):
-
-    stats = get_map_stats(map_name)
-
-    assert stats is not None
-
-
-def test_get_map_stats_for_map():
-
-    q = Replay.map_name == "Ultralove"
+def test_get_map_stats_for_map(map_name):
+    q = Replay.map_name == map_name
 
     maps: list[MatchupsByMap] = replaydb.db.find_many(Model=MatchupsByMap, query=q)
 
     print(maps)
     assert len(maps) > 0
     assert len(maps[0].matchups) > 0
+
+
+@pytest.mark.parametrize(
+    "map_name",
+    config.ladder_maps,
+)
+def test_get_season_map_stats(map_name):
+    stats = get_season_map_stats(map_name)
+
+    assert stats is not None
+    assert stats.matchups is not None
+    assert len(stats.matchups) > 0
+    print(stats)
