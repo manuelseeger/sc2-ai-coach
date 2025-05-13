f57a5e510649e0ce017ac24f6cfcf4e28de28a9f
manuelseeger <mail@manuelseeger.de>
2025-05-13 10:00:56 +0200

    Remove unecessary mocks

--- tests/llm/test_critic_replay.py
+++ tests/llm/test_critic_replay.py
@@ -1,25 +1,19 @@
 import parametrize_from_file
-import pytest
 
 from coach import AISession
 from src.replaydb.reader import ReplayReader
 from tests.conftest import only_in_debugging
 from tests.critic import LmmCritic
-from tests.mocks import MicMock, TranscriberMock, TTSMock
 
 
 @only_in_debugging
 @parametrize_from_file(indirect=["replay_file"])
-def test_analyze_replay(replay_file, instructions, convo, mocker, critic: LmmCritic):
+def test_analyze_replay(
+    replay_file, instructions, convo, critic: LmmCritic, sc2api_mock
+):
     # arrange
     reader = ReplayReader()
     session = AISession()
-
-    user_convo = [c["question"] for c in convo]
-
-    mocker.patch("coach.mic", MicMock())
-    mocker.patch("coach.tts", TTSMock())
-    mocker.patch("coach.transcriber", TranscriberMock(data=user_convo))
 
     replay = reader.load_replay(replay_file)
     session.initiate_from_new_replay(replay)
