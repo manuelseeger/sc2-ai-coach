f029e178e91cc424595b059085663c2d9a4c9c4f
manuelseeger <mail@manuelseeger.de>
2026-05-14 09:57:43 +0200

    Better player tests

--- tests/unit/test_playeridentity_runtime.py
+++ tests/unit/test_playeridentity_runtime.py
@@ -1,23 +1,40 @@
 from src.runtime.playeridentity import build_player_identity_services
+from src.util import is_barcode
 
 
-def test_build_player_identity_services_shares_replay_store_and_portrait_source(
-    runtime_settings, mocker
+def _seeded_named_student_replay(seeded_replay_mongo_container):
+    runtime_settings = seeded_replay_mongo_container.settings
+    return next(
+        replay
+        for replay in seeded_replay_mongo_container.seeded_replays
+        if any(
+            player.name == runtime_settings.student.name for player in replay.players
+        )
+        and not is_barcode(replay.get_opponent_of(runtime_settings.student.name).name)
+    )
+
+
+def test_build_player_identity_services_enriches_then_resolves_from_shared_store(
+    seeded_replay_mongo_container,
 ):
-    replay_store = mocker.Mock()
-    sc2pulse = mocker.Mock()
-    sc2client = mocker.Mock()
+    runtime_settings = seeded_replay_mongo_container.settings.model_copy(deep=True)
+    replay_store = seeded_replay_mongo_container.replay_store
+    replay = _seeded_named_student_replay(seeded_replay_mongo_container)
+    opponent = replay.get_opponent_of(runtime_settings.student.name)
 
     services = build_player_identity_services(
         runtime_settings,
         replay_store=replay_store,
-        sc2pulse=sc2pulse,
-        sc2client=sc2client,
     )
 
-    assert services.resolver.replay_store is replay_store
-    assert services.enricher.replay_store is replay_store
-    assert services.resolver.sc2pulse is sc2pulse
-    assert services.resolver.sc2client is sc2client
-    assert services.resolver.portrait_source is services.portrait_source
-    assert services.enricher.portrait_source is services.portrait_source
+    saved_player_info = services.enricher.save_from_replay(replay)
+    resolved_player_info = services.resolver.resolve_player(
+        opponent=opponent.name,
+        mapname=replay.map_name,
+        mmr=opponent.scaled_rating,
+    )
+
+    assert resolved_player_info is not None
+    assert resolved_player_info.id == saved_player_info.id
+    assert resolved_player_info.toon_handle == saved_player_info.toon_handle
+    assert resolved_player_info.name == opponent.name
